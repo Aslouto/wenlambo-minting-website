e6677425a3832251df75d89d1c8b050d8ceb4e03
NicoodanielB <86988527+NicoodanielB@users.noreply.github.com>
2022-01-21 00:11:47 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -249,7 +249,7 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-
+                window.open("/config/rarity.html", "_blank");
                 }}
                 style={{
                   margin: "5px",
